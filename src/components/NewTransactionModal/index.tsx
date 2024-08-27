56fe915905c063ad22bd7c027904f95fd149e091
Yaba Ernesto <yabaernesto@gmail.com>
2024-08-27 16:17:11 +0100

    feat: add element in form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from 'phosphor-react';
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 export function NewTransactionModal() {
 
@@ -19,6 +19,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descricao" required />
           <input type="number" placeholder="Preco" required />
           <input type="text" placeholder="Categorias" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant='income'>
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outcome'>
+              <ArrowCircleDown size={24} />
+              Saida
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">
             Cadastrar
